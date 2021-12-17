e39192027f74f9de612ca9ad69f66f20424a02d0
EliasPereyra <EliasPereyra_Gomez@hotmail.com>
2021-12-17 18:59:43 -0300

    Some issues are yet to be solved in the Map component.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { CssBaseline, Grid } from "@material-ui/core";
 
-import { getPlacesData } from "./api/travelAdvisorAPI";
+import { getPlacesData, getWeatherData } from "./api/travelAdvisorAPI";
 import Header from "./components/Header/Header";
 import List from "./components/List/List";
 import Map from "./components/Map/Map";
@@ -15,6 +15,7 @@ const App = () => {
 
   const [places, setPlaces] = useState([]);
   const [filteredPlaces, setFilteredPlaces] = useState([]);
+  const [weatherData, setWeatherData] = useState([])
 
   const [autocomplete, setAutocomplete] = useState(null);
   const [childClicked, setChildClicked] = useState(null);
@@ -37,6 +38,9 @@ const App = () => {
   useEffect(() => {
     if (bounds) {
       setIsLoading(true);
+
+      getWeatherData(coords.lat, coords.lng)
+        .then((data) => setWeatherData(data))
 
       getPlacesData(type, bounds.sw, bounds.ne).then((data) => {
         setPlaces(data.filter((place) => place.name && place.num_reviews > 0));
@@ -76,14 +80,21 @@ const App = () => {
       <Grid
         item
         xs={12}
-        md={4}
+        md={8}
         style={{
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
         }}
       >
-        <Map />
+        <Map
+          setChildClicked={setChildClicked}
+          setBounds={setBounds}
+          setCoords={setCoords}
+          coords={coords}
+          places={filteredPlaces.length ? filteredPlaces : places}
+          weatherData={weatherData}
+        />
       </Grid>
     </>
   );
--- src/api/travelAdvisorAPI.js
+++ src/api/travelAdvisorAPI.js
@@ -1,8 +1,12 @@
-import axios from 'axios'
+import axios from "axios";
 
-export const getPlacesData = async(type, sw, ne) => {
-    try {
-        const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
         params: {
           bl_latitude: sw.lat,
           bl_longitude: sw.lng,
@@ -10,20 +14,33 @@ export const getPlacesData = async(type, sw, ne) => {
           tr_latitude: ne.lat,
         },
         headers: {
-          'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
-          'x-rapidapi-key': process.env.TRAVEL_ADVISOR_API_KEY
-        }
-    })
+          "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+          "x-rapidapi-key": process.env.TRAVEL_ADVISOR_API_KEY,
+        },
+      }
+    );
 
     return data;
-    } catch (error) {
-        console.log(error)
-    }
-}
-export const getWeatherData = async () => {
-  try {
-    
   } catch (error) {
-    
+    console.log(error);
   }
-}
+};
+export const getWeatherData = async (lat, lng) => {
+  try {
+    if (lat && lng) {
+      const { data } = await axios.get(
+        "https://community-open-weather-map.p.rapidapi.com/find",
+        {
+          params: { lat, long: lng },
+          headers: {
+            "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+            "x-rapidapi-key": process.env.OPEN_WEATHER_MAP_API_KEY,
+          },
+        }
+      );
+      return data;
+    }
+  } catch (error) {
+    console.log(error)
+  }
+};
