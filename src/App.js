24986b70f13d52a8c3b7da98334a9b87dccd5fb6
EliasPereyra <EliasPereyra_Gomez@hotmail.com>
2021-12-16 23:56:56 -0300

    The Map component is in progress.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Map from "./components/Map/Map";
 
 const App = () => {
   const [type, setType] = useState("restaurants");
-  const [rating, setRating] = useState('');
+  const [rating, setRating] = useState("");
 
   const [coords, setCoords] = useState([]);
   const [bounds, setBounds] = useState(null);
@@ -17,7 +17,7 @@ const App = () => {
   const [filteredPlaces, setFilteredPlaces] = useState([]);
 
   const [autocomplete, setAutocomplete] = useState(null);
-  const [childClicked, setChildClicked] = useState(null)
+  const [childClicked, setChildClicked] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
@@ -29,25 +29,25 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-      const filtered = places.filter((place) => Number(place.rating) > rating)
-      
-      setFilteredPlaces(filtered)
+    const filtered = places.filter((place) => Number(place.rating) > rating);
+
+    setFilteredPlaces(filtered);
   }, [rating]);
 
   useEffect(() => {
     if (bounds) {
-        setIsLoading(true);
+      setIsLoading(true);
 
       getPlacesData(type, bounds.sw, bounds.ne).then((data) => {
         setPlaces(data.filter((place) => place.name && place.num_reviews > 0));
         setFilteredPlaces([]);
-        setRating('')
+        setRating("");
         setIsLoading(false);
       });
     }
   }, [bounds, type]);
 
-  const onLoad = (autoC) => setAutocomplete(autoC)
+  const onLoad = (autoC) => setAutocomplete(autoC);
 
   const onPlaceChanged = () => {
     const lat = autocomplete.getPlace().geometry.geolocation.lat();
@@ -62,15 +62,15 @@ const App = () => {
       <Header onPlaceChanged={onPlaceChanged} onLoad={onLoad} />
       <Grid container spacing={3} style={{ width: "100%" }}>
         <Grid item xs={12} md={4}>
-          <List 
+          <List
             isLoading={isLoading}
             childClicked={childClicked}
-            places={filteredPlaces.length ? filteredPlaces : places} 
+            places={filteredPlaces.length ? filteredPlaces : places}
             type={type}
             setType={setType}
             rating={rating}
             setRating={setRating}
-        />
+          />
         </Grid>
       </Grid>
       <Grid
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,13 +1,36 @@
+import GoogleMapReact from 'google-map-react'
+
 import { Paper, Typography, useMediaQuery } from '@material-ui/core'
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined'
 
+import mapStyles from '../../mapStyles'
 import useStyles from './styles'
 
-const Map = () => {
+const Map = ({coords, places, setCoords, setBounds, setChildClicked }) => {
     const classes = useStyles();
+    const matches = useMediaQuery('')
 
     return (
         <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{key: process.env.GOOGLE_MAPS_API_KEY}}
+                defaultCenter={coords}
+                center={coords}
+                defaultZoom={14}
+                margin={[50,50,50,50]}
+                options={{disableDefaultUI:true, zoomControl: true, styles: mapStyles }}
+                onChange={(e) => {
+                    setCoords({ lat: e.center.lat, lng: e.center.lng})
+                    setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw})
+                }}
+                onChildClick={(child) => setChildClicked(child)}
+            >
+                {places.length && places.map((place,i) => (
+                    <div className={classes.markerContainer} lat={Number(place.latitude)} lng={Number(place.longitude)} ley={i}>
+                        {!matches}
+                    </div>
+                ))}
+            </GoogleMapReact>
         </div>
     )
 }
